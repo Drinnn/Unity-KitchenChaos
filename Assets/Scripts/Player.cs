803745cb260a5256e65debd6d44e911019b4e2e0
Drinnn <pedro_lontro@hotmail.com>
2023-05-13 16:49:58 -0300

    feat: add clear counter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,48 @@ public class Player : MonoBehaviour
     [SerializeField] private float playerHeight = 2f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float interactionDistance = 2f;
+    [SerializeField] private LayerMask countersLayerMask;
 
     [SerializeField] private GameInput gameInput;
 
     public bool IsWalking { get; private set; }
+
+    private Vector3 _lastInteractDir;
     
     private void Update()
     {
         HandleMovement();
+        HandleInteractions();
+    }
+
+    private Vector3 GetMovementDirection()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
+        return new Vector3(inputVector.x, 0f, inputVector.y);
+    }
+
+    private void HandleInteractions()
+    {
+        Vector3 moveDir = GetMovementDirection();
+        if (moveDir != Vector3.zero)
+        {
+            _lastInteractDir = moveDir;
+        }
+
+        if (Physics.Raycast(transform.position, _lastInteractDir, out RaycastHit raycastHit, interactionDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();                   
+            }
+        }
     }
 
     private void HandleMovement()
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        Vector3 moveDir = GetMovementDirection();
         float moveDistance = moveSpeed * Time.deltaTime;        
         
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
@@ -55,4 +83,5 @@ public class Player : MonoBehaviour
 
         IsWalking = moveDir != Vector3.zero;
     }
+    
 }
