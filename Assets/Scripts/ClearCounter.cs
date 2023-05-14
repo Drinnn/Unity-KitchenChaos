7043764b4ae70291909ac7f5bfda4af62707f16e
Drinnn <pedro_lontro@hotmail.com>
2023-05-14 11:38:12 -0300

    feat: add kitchen object smart parent switching

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,14 +4,31 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private Transform topPoint;
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    
+
+    private KitchenObject _kitchenObject;
+
+    public Transform TopPoint
+    {
+        get => topPoint;
+    }
+    public KitchenObject KitchenObject
+    {
+        get => _kitchenObject;
+        set => _kitchenObject = value;
+    }
+
+    public bool HasKitchenObject
+    {
+        get => _kitchenObject != null;
+    }
+
     public void Interact()
     {
-        Debug.Log("Interact");
-        
-        GameObject kitchenObject = Instantiate(kitchenObjectSO.prefab, topPoint);
-        kitchenObject.transform.localPosition = Vector3.zero;
-        
-        Debug.Log(kitchenObject.GetComponent<KitchenObject>().KitchenObjectSO.objectName);
+        if (_kitchenObject == null)
+        {
+            GameObject kitchenObject = Instantiate(kitchenObjectSO.prefab, topPoint);
+            kitchenObject.transform.localPosition = Vector3.zero;
+            kitchenObject.GetComponent<KitchenObject>().ClearCounter = this;
+        }
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -3,9 +3,33 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO _kitchenObjectSO;
-
+    
     public KitchenObjectSO KitchenObjectSO
     {
         get => _kitchenObjectSO;
+    }
+
+    private ClearCounter _clearCounter;
+    public ClearCounter ClearCounter
+    {
+        get => _clearCounter;
+        set
+        {
+            if (_clearCounter != null)
+            {
+                _clearCounter.KitchenObject = null;
+            }
+            
+            _clearCounter = value;
+
+            if (_clearCounter.HasKitchenObject)
+            {
+                Debug.LogError("Counter already has a KitchenObject!");
+            }
+            _clearCounter.KitchenObject = this;
+
+            transform.parent = _clearCounter.TopPoint;
+            transform.localPosition = Vector3.zero;
+        }
     }
 }
