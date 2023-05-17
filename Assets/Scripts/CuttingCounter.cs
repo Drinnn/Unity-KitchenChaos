c7db2418b8a2138016f8fa0cac68e5133da14931
Drinnn <pedro_lontro@hotmail.com>
2023-05-17 15:24:32 -0300

    feat: add progress bar with UI

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,8 +1,20 @@
+using System;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+    public class OnProgressChangedEventArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+
+    public event EventHandler OnCut;
+    
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
+
+    private int _cuttingProgress;
+    
     public override void Interact(Player player)
     {
         if (!HasKitchenObject())
@@ -10,6 +22,13 @@ public class CuttingCounter : BaseCounter
             if (player.HasKitchenObject() && HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
             {
                 player.GetKitchenObject().SetKitchenObjectParent(this);
+                _cuttingProgress = 0;
+                
+                CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+                OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs
+                {
+                    progressNormalized = (float)_cuttingProgress / cuttingRecipeSO.cuttingProgressMax
+                });
             }
         }
         else
@@ -25,35 +44,55 @@ public class CuttingCounter : BaseCounter
     {
         if (HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO()))
         {
-            var outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
-            GetKitchenObject().DestroySelf();
-            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+            _cuttingProgress++;
+            
+            CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+            
+            OnCut?.Invoke(this, EventArgs.Empty);
+            OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs
+            {
+                progressNormalized = (float)_cuttingProgress / cuttingRecipeSO.cuttingProgressMax
+            });
+            
+            if (_cuttingProgress >= cuttingRecipeSO.cuttingProgressMax)
+            {
+                var outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+            
+                GetKitchenObject().DestroySelf();
+            
+                KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+            }
         }
     }
 
     private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
     {
-        foreach (var cuttingRecipeSO in cuttingRecipeSOArray)
-        {
-            if (cuttingRecipeSO.input == inputKitchenObjectSO)
-            {
-                return true;
-            }
-        }
+        CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(inputKitchenObjectSO);
 
-        return false;
+        return cuttingRecipeSO != null;
     }
 
     private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(inputKitchenObjectSO);
+        if (cuttingRecipeSO)
+        {
+            return cuttingRecipeSO.output;
+        }
+
+        return null;
+    }
+
+    private CuttingRecipeSO GetCuttingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
     {
         foreach (var cuttingRecipeSO in cuttingRecipeSOArray)
         {
             if (cuttingRecipeSO.input == inputKitchenObjectSO)
             {
-                return cuttingRecipeSO.output;
+                return cuttingRecipeSO;
             }
         }
 
         return null;
-    } 
+    }
 }
---
+++ Assets/Scripts/CuttingCounterVisual.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class CuttingCounterVisual : MonoBehaviour
+{
+    [SerializeField] private CuttingCounter cuttingCounter;
+
+    private const string CUT = "Cut";
+    
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        cuttingCounter.OnCut += CuttingCounter_OnCut;
+    }
+
+    private void CuttingCounter_OnCut(object sender, EventArgs e)
+    {
+        _animator.SetTrigger(CUT);
+    }
+}
